c9943fc6755ef1c88c6343c7ab57b40bdf7ddba3
Yonatan Peters <yonipeters7@gmail.com>
2025-10-25 15:36:50 -0400

    task lister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+  const taskDescription = event.target["new-task-description"].value;
+  const taskObject = {description: taskDescription};
+
+  buildToDo(taskObject);
+
+  event.target.reset();
+  });
 });
+
+
+  function buildToDo(taskObject) {
+    const tasksList = document.getElementById("tasks");
+    const newTaskItem = document.createElement("li");
+    newTaskItem.textContent = taskObject.description;
+    tasksList.append(newTaskItem);
+  }
+  
